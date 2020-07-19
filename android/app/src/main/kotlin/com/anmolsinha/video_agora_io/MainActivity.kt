f7c17ae79281820d429f951b08266379fa6bd584
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-07-19 20:33:24 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/anmolsinha/video_agora_io/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anmolsinha.video_agora_io
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = 'e7c5df04e2b14354b83a4e6c4acbfc7e';
